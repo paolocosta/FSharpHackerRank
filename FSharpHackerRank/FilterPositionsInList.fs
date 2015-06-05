e79b87ac23cf0192157e614c3582f4b9c5b238b0
Paolo Costa <paolo.enrico.costa@gmail.com>
2015-06-05 05:27:32 +0200

    start

---
+++ FSharpHackerRank/FilterPositionsInList.fs
@@ -0,0 +1,26 @@
+﻿module FilterPositionsInList
+
+    let FilterPositionsInList  =
+        let rec procinput lines=
+            match System.Console.ReadLine() |> System.Int32.TryParse with
+            | false, _ -> List.rev lines
+            | true, n -> procinput (n::lines)
+
+        let values = procinput []
+
+        let rest lst =
+            match lst with
+            | h::t -> t
+            | _ -> []
+
+        let onlyOdd lines =
+            let rec onlyOdd2 lines odd =
+                match odd, lines with
+                    | _, [] -> []
+                    | true, l  ->  (List.head l) :: (onlyOdd2 (rest l) false)
+                    | false, l -> onlyOdd2 (rest l) true
+
+            onlyOdd2 lines false
+
+        values
+        |> onlyOdd
---
+++ FSharpHackerRank/Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv =
+    FilterPositionsInList.FilterPositionsInList  // return an integer exit code
+    |> List.iter (printfn "%d")
+
+    ignore(System.Console.ReadLine())
+
+    0
