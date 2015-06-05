241f0fc2cca624d3e901ab1837f16fcae6e48e73
Paolo Costa <paolo.enrico.costa@gmail.com>
2015-06-05 05:57:16 +0200

    test project is present

---
+++ FSharpHackerRank.Test/FilterPositionsInListTest.fs
@@ -0,0 +1,16 @@
+﻿module Test
+
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``should an empty string return an_empty string``() =
+    FilterPositionsInList.FilterPositionsInList [] |> should equal  []
+
+[<Test>]
+let ``should a single element string return itself``() =
+    FilterPositionsInList.FilterPositionsInList [1] |> should equal [1]
+
+[<Test>]
+let ``should work correctly with a long list``() =
+    FilterPositionsInList.FilterPositionsInList [1;2;3;4] |> should equal [1;3]
---
+++ FSharpHackerRank/Utils.fs
@@ -0,0 +1,11 @@
+﻿module Utils
+
+    let rec procinput lines=
+            match System.Console.ReadLine() |> System.Int32.TryParse with
+            | false, _ -> List.rev lines
+            | true, n -> procinput (n::lines)
+
+    let rest lst =
+            match lst with
+            | h::t -> t
+            | _ -> []
