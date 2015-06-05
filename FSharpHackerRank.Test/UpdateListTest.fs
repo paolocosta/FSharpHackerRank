85d12c5c159957cfd32f8f5136efd3c6ccb85301
Paolo Costa <paolo.enrico.costa@gmail.com>
2015-06-05 21:33:24 +0200

    UpdateList

---
+++ FSharpHackerRank.Test/UpdateListTest.fs
@@ -0,0 +1,12 @@
+﻿module UpdateListTest
+
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``should empty list return empty list``() =
+    UpdateList.updateList [] |> should equal []
+
+[<Test>]
+let ``should work correctly``() =
+    UpdateList.updateList [1;1;-10;4;2;1;-56] |> should equal [1;1;10;4;2;1;56]
---
+++ FSharpHackerRank/UpdateList.fs
@@ -0,0 +1,4 @@
+﻿module UpdateList
+
+let updateList (l:int list) =
+    List.map (fun element -> System.Math.Abs(element:int)) l
