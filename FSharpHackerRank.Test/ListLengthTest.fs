a17b85a1be89f6eb7fa7d0cc4e3ee8ba34af0d2b
Paolo Costa <paolo.enrico.costa@gmail.com>
2015-06-05 21:21:01 +0200

    ListLength

---
+++ FSharpHackerRank.Test/ListLengthTest.fs
@@ -0,0 +1,16 @@
+﻿module ListLengthTest
+
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``should empty list return 0``() =
+    ListLength.listLength [] |> should equal 0
+
+[<Test>]
+let ``should single list return 1``() =
+    ListLength.listLength [1] |> should equal 1
+
+[<Test>]
+let ``should long list return the length``() =
+    ListLength.listLength [1;1;1;1;1] |> should equal 5
---
+++ FSharpHackerRank/ListLength.fs
@@ -0,0 +1,6 @@
+﻿module ListLength
+
+let listLength list =
+    let elementSoFar = 0
+    list
+    |> List.fold (fun acc elem -> acc + 1) 0
