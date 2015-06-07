54cc28f2ce37803e8c74086f53232d89963fb8df
Paolo Costa <paolo.enrico.costa@gmail.com>
2015-06-07 22:22:35 +0200

    GDC

---
+++ FSharpHackerRank.Test/ComputingTheGDCTest.fs
@@ -0,0 +1,16 @@
+﻿module ComputingTheGDCTest
+
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``should calculate GCD of 1 5 give 1``() =
+    ComputingTheGDC.main "1 5"  |> should equal 1
+
+[<Test>]
+let ``should calculate GCD of 10 100 give 10``() =
+    ComputingTheGDC.main "10 100"  |> should equal 10
+
+[<Test>]
+let ``should calculate GCD of 22 131 give 10``() =
+    ComputingTheGDC.main "22 131"  |> should equal 1
---
+++ FSharpHackerRank/ComputingTheGDC.fs
@@ -0,0 +1,16 @@
+﻿module ComputingTheGDC
+
+let extractList (s:string) =
+    s.Split(' ')
+    |>  Array.toList
+    |> List.map System.Int32.Parse
+
+let rec GDC (X:int) (Y:int) : int =
+    if X = Y then X
+    else if X > Y then (GDC (X - Y) Y)
+    else (GDC (Y - X) X)
+
+let main i1  =
+    let values = extractList(i1)
+
+    GDC (List.nth values 0) (List.nth values 1)   
