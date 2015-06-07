1858537f6a43718c97b3f6fc00c12d7fceeb9a58
Paolo Costa <paolo.enrico.costa@gmail.com>
2015-06-07 21:11:40 +0200

    AreaUnderCurvesAndVolumeOfRevolvingACurve

---
+++ FSharpHackerRank.Test/AreaUnderCurvesAndVolumeOfRevolvingACurveTest.fs
@@ -0,0 +1,34 @@
+﻿module AreaUnderCurvesAndVolumeOfRevolvingACurveTest
+
+open NUnit.Framework
+open FsUnit
+
+let isRelativeDifferenceMinus001 t =
+    System.Math.Abs((float (fst t)) / (float (snd t))) > 0.99
+
+[<Test>]
+let ``should extract list work correctly``() =
+    AreaUnderCurvesAndVolumeOfRevolvingACurve.extractList "1 2"  |> should equal [1.0;2.0]
+
+[<Test>]
+let ``should main work correctly with a complex text case``() =
+        let result = (AreaUnderCurvesAndVolumeOfRevolvingACurve.main
+                "-1 2 0 2 -1 -3 -4 -1 -3 -4 -999 1 2 3 4 5 1 2 0 2 -1 -3 -4 -1 -3 -4 -999 1 2 3 4 5"
+                "-16 -15 -14 -13 -12 -11 -10 -9 -8 -7 -6 -5 -4 -3 -2 -1 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15"
+                "1 2")
+        isRelativeDifferenceMinus001 (fst result, -152853.7) |> should equal true
+        isRelativeDifferenceMinus001 (snd result, 196838966733.0) |> should equal true
+
+[<Test>]
+let ``should main work correctly with a simple text case``() =
+        let result = AreaUnderCurvesAndVolumeOfRevolvingACurve.main "1 2 3 4 5" "6 7 8 9 10" "1 4"
+        isRelativeDifferenceMinus001 (fst result, 2435300.3) |> should equal true
+        isRelativeDifferenceMinus001 (snd result, 26172951168940.8) |> should equal true
+
+[<Test>]
+let ``should get value work correctly with a two elements list``() =
+    AreaUnderCurvesAndVolumeOfRevolvingACurve.GetPolinomValue [0.0;1.0] [-1.0;2.0] 1.0 |> should equal 1.0
+
+[<Test>]
+let ``should get value work correctly with a three elements list``() =
+    AreaUnderCurvesAndVolumeOfRevolvingACurve.GetPolinomValue [1.0;2.0;3.0] [1.0;2.0;3.0] 2.0 |> should equal 34.0
---
+++ FSharpHackerRank/AreaUnderCurvesAndVolumeOfRevolvingACurve.fs
@@ -0,0 +1,39 @@
+﻿module AreaUnderCurvesAndVolumeOfRevolvingACurve
+
+let extractList (s:string) =
+    s.Split(' ')
+    |>  Array.toList
+    |> List.map System.Int32.Parse
+    |> List.map float
+
+let tenRaised exponent = 10.0 ** float exponent
+
+let GetPolinomValue multipliers exponents x =
+    let calc mult exp =
+        match mult with
+        |0.0 -> 0.0
+        |_ -> mult * (x ** exp)
+    [0.. (List.length multipliers)-1]
+    |> List.fold (
+        fun acc elem ->
+            acc +
+                calc (float(List.nth multipliers elem)) (float(List.nth exponents elem))) 0.0
+
+let fineList L R grane = [ for i in L * (tenRaised grane) + ( 1.0 / (tenRaised grane)) .. R * (tenRaised grane)  -> i / (tenRaised grane)]
+
+let GetArea multipliers exponents L R precision =
+    fineList L R precision
+    |> List.fold (fun acc elem -> acc + (GetPolinomValue multipliers exponents elem)/ (10.0 ** float precision)) 0.0
+
+let GetVolume multipliers exponents L R precision =
+    fineList L R precision
+    |> List.fold (fun acc elem -> acc + System.Math.PI * System.Math.Abs((GetPolinomValue multipliers exponents elem) ** 2.0 / (10.0 ** float precision) )) 0.0
+
+let main i1 i2 i3 =
+    let multipliers = extractList(i1)
+    let exponents = extractList (i2)
+    let values = extractList (i3)
+    let L = List.nth values 0
+    let R = List.nth values 1
+
+    GetArea multipliers exponents L R 3,GetVolume multipliers exponents L R 3
