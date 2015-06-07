8cbe74c4ba499f02b3a8f5bcc2ea12f07a64c793
Paolo Costa <paolo.enrico.costa@gmail.com>
2015-06-07 22:35:02 +0200

    better GDC

--- FSharpHackerRank/ComputingTheGDC.fs
+++ FSharpHackerRank/ComputingTheGDC.fs
@@ -1,10 +1,5 @@
 ﻿module ComputingTheGDC
 
-let extractList (s:string) =
-    s.Split(' ')
-    |> Array.toList
-    |> List.map System.Int32.Parse
-
 let rec GDC X Y =
     if X = Y then X
     else if X > Y then (GDC (X - Y) Y)
