e491a25163c827a31dce13e8f34bf8aa863ea167
Paolo Costa <paolo.enrico.costa@gmail.com>
2015-06-07 22:34:39 +0200

    better GDC

--- FSharpHackerRank.Test/ComputingTheGDCTest.fs
+++ FSharpHackerRank.Test/ComputingTheGDCTest.fs
@@ -5,12 +5,12 @@ open FsUnit
 
 [<Test>]
 let ``should calculate GCD of 1 5 give 1``() =
-    ComputingTheGDC.main "1 5"  |> should equal 1
+    ComputingTheGDC.GDC 1 5  |> should equal 1
 
 [<Test>]
 let ``should calculate GCD of 10 100 give 10``() =
-    ComputingTheGDC.main "10 100"  |> should equal 10
+    ComputingTheGDC.GDC 10 100  |> should equal 10
 
 [<Test>]
 let ``should calculate GCD of 22 131 give 10``() =
-    ComputingTheGDC.main "22 131"  |> should equal 1
+    ComputingTheGDC.GDC 22 131  |> should equal 1
--- FSharpHackerRank/ComputingTheGDC.fs
+++ FSharpHackerRank/ComputingTheGDC.fs
@@ -5,14 +5,7 @@ let extractList (s:string) =
     |> Array.toList
     |> List.map System.Int32.Parse
 
-let rec GDC (X:int) (Y:int) : int =
+let rec GDC X Y =
     if X = Y then X
     else if X > Y then (GDC (X - Y) Y)
     else (GDC (Y - X) X)
-
-[<EntryPoint>]
-let main args  =
-    let i1 = System.Console.ReadLine()
-    let values = extractList(i1)
-    printfn "%d" (GDC (List.nth values 0) (List.nth values 1))
-    0
